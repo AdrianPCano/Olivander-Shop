6f1e4198453367d01aa345ba1ec11fd4d18e1513
Adrian Ponce Cano <adrianponcecano@gmail.com>
2017-12-04 23:02:09 +0100

    añadir prints 3

--- OlivanderShop.py
+++ OlivanderShop.py
@@ -46,7 +46,7 @@ def crearFicheroCasosTest(ficheroVolcadoCasosTest, matrizCasosTest):
             print("La ruta de acceso al fichero ha de ser un string")
     else:
         for (offset, casosTestDia) in enumerate(matrizCasosTest):
-            stdout.write('-' * 5 + " Dia %d: " % offset + '-' * 5 + '\n')
+            print('-' * 5 + " Dia %d: " % offset + '-' * 5)
             for item in casosTestDia:
                 stdout.write(','.join(item) + '\n')
         stdout.close()
