48ad292e53b96268bb1ccf61b11e44f9b0fab398
Adrian Ponce Cano <adrianponcecano@gmail.com>
2017-12-04 21:29:13 +0100

    arreglo antiguos comentarios

--- OlivanderShop.py
+++ OlivanderShop.py
@@ -63,7 +63,7 @@ def mostrarCasosTest(matrizCasosTest):
     : matrizCasosTest: Matriz con los elementos de la tienda separados por ','.
     : return: En este caso no devuelve nada, imprime por pantalla los objetos de la tienda en forma de lista.
 
-    ""
+    """
 
     for (offset, casosTestDia) in enumerate(matrizCasosTest):
         print('-' * 5 + " Dia %d: " % offset + '-' * 5)
